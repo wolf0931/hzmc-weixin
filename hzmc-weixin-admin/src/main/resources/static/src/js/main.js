faecde1430478eefb1c39d03d094221760363078
dusj <dusj@mchz.com.cn>
2017-05-17 15:47:58 +0800

    PC页面

--- hzmc-weixin-admin/src/main/resources/static/src/js/main.js
+++ hzmc-weixin-admin/src/main/resources/static/src/js/main.js
@@ -38,18 +38,18 @@ angular.module('myApp',[]).controller('myCtrl',function($scope){
 			success: function(data){
 		
 				for(var i=0;i<data.data.length;i++){
-					if(data.data[i].wxPayRecord.status == 'SEDING'){
-						data.data[i].wxPayRecord.status = '发放中';
-					}else if(data.data[i].wxPayRecord.status == 'SENT'){
-						data.data[i].wxPayRecord.status = '已发放待领取';
-					}else if(data.data[i].wxPayRecord.status == 'FAILED'){
-						data.data[i].wxPayRecord.status = '发放失败';
-					}else if(data.data[i].wxPayRecord.status == 'RECEIVED'){
-						data.data[i].wxPayRecord.status = '已领取';
-					}else if(data.data[i].wxPayRecord.status == 'RFUND_ING'){
-						data.data[i].wxPayRecord.status = '退款中';
-					}else if(data.data[i].wxPayRecord.status == 'REFUND'){
-						data.data[i].wxPayRecord.status = '已退款';
+					if(data.data[i].rows.wxPayRecord.status == 'SEDING'){
+						data.data[i].rows.wxPayRecord.status = '发放中';
+					}else if(data.data[i].rows.wxPayRecord.status == 'SENT'){
+						data.data[i].rows.wxPayRecord.status = '已发放待领取';
+					}else if(data.data[i].rows.wxPayRecord.status == 'FAILED'){
+						data.data[i].rows.wxPayRecord.status = '发放失败';
+					}else if(data.data[i].rows.wxPayRecord.status == 'RECEIVED'){
+						data.data[i].rows.wxPayRecord.status = '已领取';
+					}else if(data.data[i].rows.wxPayRecord.status == 'RFUND_ING'){
+						data.data[i].rows.wxPayRecord.status = '退款中';
+					}else if(data.data[i].rows.wxPayRecord.status == 'REFUND'){
+						data.data[i].rows.wxPayRecord.status = '已退款';
 					}
 				}
 				$scope.list = data;
