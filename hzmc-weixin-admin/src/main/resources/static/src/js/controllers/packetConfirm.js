7129e38f2d46da1c35f6d22b035681e779766e98
dusj <dusj@mchz.com.cn>
2017-05-18 10:04:54 +0800

    移动端页面

--- hzmc-weixin-admin/src/main/resources/static/src/js/controllers/packetConfirm.js
+++ hzmc-weixin-admin/src/main/resources/static/src/js/controllers/packetConfirm.js
@@ -15,131 +15,15 @@ function config(){
     $('#rate').val('');
     $('#introduce').val('');
     $('#wishing').val('');
-    $('#submit').html('提交');
+    $('#submit').removeClass('display');
+    $('#update').addClass('display');
 }
 
 
 $('#submit').click(function(){
     event.preventDefault();
-    $sendName=$('#sendName').val();
-    $actName=$('#activeName').val();
-    $sum=$('#sum').val();
-    $number=$('#number').val();
-    $minAccount=$('#minAccount').val();
-    $maxAccount=$('#maxAccount').val();
-    $startDate=Date.parse($('#startDate').val())/1000;
-    $endDate=Date.parse($('#endDate').val())/1000;
-    $rate=$('#rate').val();
-    $introduce=$('#introduce').val();
-    $wishing=$('#wishing').val();
-    
-    if($sendName == ''){
-    	emptywarn($('#sendName'),'商户名');
+    if(inputJuge() == false){
     	return ;
-    }else{
-    	$('.errwarn').remove();
-    }
-    
-    if($actName == ''){
-    	emptywarn($('#activeName'),'活动名');
-    	return ;
-    }else{
-    	$('.errwarn').remove();
-    }
-    
-    if($sum == ''){
-    	emptywarn($('#sum'),'总金额');
-    	return ;
-    }else if(isNaN($sum) || $sum < 0){
-    	typewarn($('#sum'),'总金额');
-    	return ;
-    }else{
-    	$('.errwarn').remove();
-    }
-    
-    if($number == ''){
-    	emptywarn($('#number'),'红包总数');
-    	return ;
-    }else if(isNaN($number) || $number < 0){
-    	typewarn($('#number'),'红包总数');
-    	return ;
-    }else if($number > $sum){
-    	let warn='<span class="red errwarn">红包总数应小于红包总金额</sapn>';
-    	$('#number') .after(warn);
-    	return ;
-    }else{
-    	$('.errwarn').remove();
-    }
-    
-    if($minAccount == ''||$maxAccount == ''){
-    	emptywarn($('#maxAccount'),'金额范围');
-    	return ;
-    }else if(isNaN($minAccount) || isNaN($maxAccount)){
-    	typewarn($('#maxAccount'),'金额范围');
-    	return ;
-    }else if($minAccount < 1){
-    	let warn='<span class="red errwarn">单个红包最小金额应大于1元</sapn>';
-    	$('#maxAccount').after(warn);
-    	return ;
-    }else if($maxAccount > $sum-$number+1){
-    	let warn='<span class="red errwarn">红包最大金额应保证单个红包金额大于1元</sapn>';
-    	$('#maxAccount').after(warn);
-    	return ;
-    }else if($maxAccount < $minAccount){
-    	let warn='<span class="red errwarn">红包最大金额应大于最小金额</sapn>';
-    	$('#maxAccount').after(warn);
-    	return ;
-    }else{
-    	$('.errwarn').remove();
-    }
-    
-    if($startDate == ''||$endDate == ''){
-    	emptywarn($('#endDate'),'活动起止时间');
-    	return ;
-    }else if($endDate < $startDate){
-    	let warn='<span class="red errwarn">活动截止时间应晚于开始时间</sapn>';
-    	$('#endDate').after(warn);
-    	return ;
-    }else if($startDate < Date.parse(new Date())/1000){
-    	let warn='<span class="red errwarn">不能从过去开始</sapn>';
-    	$('#endDate').after(warn);
-    	return ;
-    }else{
-    	$('.errwarn').remove();
-    }
-    
-    if($rate == ''){
-    	emptywarn($('#rate'),'中奖几率');
-    	return ;
-    }else if(isNaN($rate) || $rate < 0 || $rate > 1){
-    	typewarn($('#rate'),'中奖几率');
-    	return ;
-    }else{
-    	$('.errwarn').remove();
-    }
-    
-    if($introduce == ''){
-    	emptywarn($('#introduce'),'活动说明');
-    	return ;
-    }else{
-    	$('.errwarn').remove();
-    }
-    
-    if($wishing == ''){
-    	emptywarn($('#wishing'),'祝福语');
-    	return ;
-    }else{
-    	$('.errwarn').remove();
-    }
-    
-    function emptywarn(obj,str){
-    	let warn='<span class="red errwarn">'+str+'不得为空</sapn>';
-    	obj.after(warn);
-    }
-    
-    function typewarn(obj,str){
-    	let warn='<span class="red errwarn">'+str+'格式错误</sapn>';
-    	obj.after(warn);
     }
     
     var redPacketTemp = {
@@ -182,6 +66,49 @@ $('#submit').click(function(){
         $('#name').val('');
     });
 });
+
+$('#update').click(function(){
+	
+    event.preventDefault();
+    if(inputJuge() == false){
+    	return ;
+    }
+
+	var redPacketTemp = {
+    		"sendName": $sendName,
+    		"actName": $actName,
+    		"totalAmount": $sum,
+    		"totalNum": $number,
+    		"minAmount": $minAccount,
+    		"maxAmount": $maxAccount,
+    		"startTime": $startDate,
+    		"endTime": $endDate,
+    		"winningRate": $rate,
+    		"remark": $introduce,
+    		"wishing": $wishing
+    };
+    $.ajax({
+    	type:'post',
+    	url:"/WxRedpackTemplet/update",
+    	dataType: "json",
+    	headers:{  
+            Accept:"application/json",  
+            "Content-Type":"application/json"  
+        },
+    	data:JSON.stringify(redPacketTemp),
+    	success:function(data){
+    		if(data.message == "success"){
+    			myAlert('更新成功');
+    		}
+    	},
+    	error:function(data){
+    		if(data.status == 401){
+    			window.location.href='../../index.html';
+    		}
+    	} 
+    });
+});
+
 
 function update(e){
 	var id = e.id;
@@ -227,7 +154,8 @@ function update(e){
 		    $('#rate').val(data.data.winningRate);
 		    $('#introduce').val(data.data.remark);
 		    $('#wishing').val(data.data.wishing);
-		    $('#submit').html('更新');
+		    $('#submit').addClass('display');
+		    $('#update').removeClass('display');
 		},
     	error:function(data){
     		if(data.status == 401){
@@ -235,6 +163,125 @@ function update(e){
     		}
     	} 
 	});
+}
+
+function inputJuge(){
+	$sendName=$('#sendName').val();
+	$actName=$('#activeName').val();
+	$sum=$('#sum').val();
+	$number=$('#number').val();
+	$minAccount=$('#minAccount').val();
+	$maxAccount=$('#maxAccount').val();
+	$startDate=Date.parse($('#startDate').val())/1000;
+	$endDate=Date.parse($('#endDate').val())/1000;
+	$rate=$('#rate').val();
+	$introduce=$('#introduce').val();
+	$wishing=$('#wishing').val();
+
+	if($sendName == ''){
+		emptywarn($('#sendName'),'商户名');
+		return false;
+	}else{
+		$('.errwarn').remove();
+	}
+
+	if($actName == ''){
+		emptywarn($('#activeName'),'活动名');
+		return false;
+	}else{
+		$('.errwarn').remove();
+	}
+
+	if($sum == ''){
+		emptywarn($('#sum'),'总金额');
+		return false;
+	}else if(isNaN($sum) || $sum < 0){
+		typewarn($('#sum'),'总金额');
+		return false;
+	}else{
+		$('.errwarn').remove();
+	}
+
+	if($number == ''){
+		emptywarn($('#number'),'红包总数');
+		return false;
+	}else if(isNaN($number) || $number < 0){
+		typewarn($('#number'),'红包总数');
+		return false;
+	}else if($number > $sum){
+		let warn='<span class="red errwarn">红包总数应小于红包总金额</sapn>';
+		$('#number') .after(warn);
+		return false;
+	}else{
+		$('.errwarn').remove();
+	}
+
+	if($minAccount == ''||$maxAccount == ''){
+		emptywarn($('#maxAccount'),'金额范围');
+		return false;
+	}else if(isNaN($minAccount) || isNaN($maxAccount)){
+		typewarn($('#maxAccount'),'金额范围');
+		return false;
+	}else if($minAccount < 1){
+		let warn='<span class="red errwarn">单个红包最小金额应大于1元</sapn>';
+		$('#maxAccount').after(warn);
+		return false;
+	}else if($maxAccount > $sum-$number+1){
+		let warn='<span class="red errwarn">红包最大金额应保证单个红包金额大于1元</sapn>';
+		$('#maxAccount').after(warn);
+		return false;
+	}else if($maxAccount < $minAccount){
+		let warn='<span class="red errwarn">红包最大金额应大于最小金额</sapn>';
+		$('#maxAccount').after(warn);
+		return false;
+	}else{
+		$('.errwarn').remove();
+	}
+
+	if($startDate == ''||$endDate == ''){
+		emptywarn($('#endDate'),'活动起止时间');
+		return false;
+	}else if($endDate < $startDate){
+		let warn='<span class="red errwarn">活动截止时间应晚于开始时间</sapn>';
+		$('#endDate').after(warn);
+		return false;
+	}else{
+		$('.errwarn').remove();
+	}
+
+	if($rate == ''){
+		emptywarn($('#rate'),'中奖几率');
+		return false;
+	}else if(isNaN($rate) || $rate < 0 || $rate > 1){
+		typewarn($('#rate'),'中奖几率');
+		return false;
+	}else{
+		$('.errwarn').remove();
+	}
+
+	if($introduce == ''){
+		emptywarn($('#introduce'),'活动说明');
+		return false;
+	}else{
+		$('.errwarn').remove();
+	}
+
+	if($wishing == ''){
+		emptywarn($('#wishing'),'祝福语');
+		return false;
+	}else{
+		$('.errwarn').remove();
+	}
+}
+
+function emptywarn(obj,str){
+	let warn='<span class="red errwarn">'+str+'不得为空</sapn>';
+	obj.after(warn);
+}
+
+function typewarn(obj,str){
+	let warn='<span class="red errwarn">'+str+'格式错误</sapn>';
+	obj.after(warn);
 }
 
 function addZreo(str){
--- hzmc-weixin-admin/src/main/resources/static/src/js/main_mobile.js
+++ hzmc-weixin-admin/src/main/resources/static/src/js/main_mobile.js
@@ -1,57 +1,46 @@
 /**
  * Created by DELL on 2017/4/25.
  */
+
+function myAlert(str){
+	 var alert='  <div class="cover"> </div><div class="add-container col-sm-4 col-sm-offset-4 no-padding">';		
+	 alert += '<div class="navbar clear"><span class="right close closeDown"></span><h4 class="left">提示</h4></div>';
+	 alert += '<div class="add-main">'+str+'</div>';
+	 alert += '<div class="alert-footer"><div class="btn-event right closeDown">确认</div></div></div>';
+	 
+	 $('body').append(alert);
+}
+
+
 var foodCount=0,customCount=0;
-$('#open1').click(function(){
-    foodCount+=1;
+
+$('.vote-button-su').click(function(){
     judge();
-    redPacket();
 });
-$('#open2').click(function(){
-    customCount+=1;
+$('.vote-button-ch').click(function(){
     judge();
-    redPacket();
 });
 
 function judge(){
-	var openId=decodeURI(location.search).split('=')[1].split('&')[0];
+//	var openId=decodeURI(location.search).split('=')[1].split('&')[0];
 	
 //	myAlert('1');
 	
 	$.ajax({
 		type: 'GET',
-    	url: '',
-    	success:function(){
-    		if(data.massage == 'success'){
-    			return;
+    	url: '/vote/1/oJvITt-VfGOTCe0dcXsZPCqn1APM',
+    	success:function(data){
+    		if(data.data == '活动已结束'){
+    			myAlert('此活动已经结束');
+    		}else if(data.data == '没有关注公众号'){
+    			myAlert('<a href="https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz='+data.data.appId+'==#wechat_redirect">先关注才能参与活动</a>');
+    		}else if(data.data == '内部人员不能发红包'){
+    			myAlert('内部人员不能领取红包');
+    		}else if(typeof(data.data) == object){
+    			myAlert('恭喜获得红包，请退回聊天窗口领取');
     		}
     	}
 	});
-	
-    $.ajax({
-    	type: 'GET',
-    	url: '/oauth/'+openId,
-    	success:function(data){
-    		if(data.message == 'success'){
-    	        myAlert('<a href="https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz='+data.data.appId+'==#wechat_redirect">先关注才能参与活动</a>');
-    	        
-    	    }
-    	}
-    });
 }
 
-function redPacket(){
-    $.ajax({
-    	type: 'GET',
-    	url: '/WxRedpackTemplet',
-    	success: function(data){
-    		$rate = data.winningRate;
-    	}
-    });
-    if(Math.random() < $rate){
-    	
-	    myAlert('恭喜获得红包，请退回聊天窗口领取');
-    }else{	
-	    myAlert('好遗憾，没有获得红包哦');
-    }
-}
+
